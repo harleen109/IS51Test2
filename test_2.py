29485274deaa3676ffb6577b7f0aff6d7e8ab145
harleen109 <harleen109@mail.fresnostate.edu>
2020-12-03 16:57:43 -0800

    finished structured english

---
+++ test_2.py
@@ -0,0 +1,9 @@
+"""
+The program will first need to read the data from the Final.txt to display the data from the Final Exam
+The program will need to count all the different exam scores of the students which means counting all the students who took the exam.
+The program will need to ouput the class average score of the exam.
+The program will need to output the percentage of students who scocred above the class average of 54.17 %
+
+Function1 will output data.split()
+Funciton2 will output sum(data) / len(data)
+Function3 will output count = count + 54.17
